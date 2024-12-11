ab05cb88d88e5ed20c67442875acee602b4bda9d
Big Sam <stosin525@gmail.com>
2024-12-11 22:24:14 +0100

    added more weather information

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const getWeather = document.querySelector('.getWeather');
+const weatherForm = document.querySelector('.getWeather');
 const container = document.querySelector('.weatherDisplay');
 const cityInput = document.querySelector('.cityName');
 const apiKey = 'f419b26d8f8702c935c8d072fa5d1bd0';
 
-getWeather.addEventListener('submit', async event => {
+
+weatherForm.addEventListener('submit', async event => {
     event.preventDefault();
 
     container.innerHTML = '';
@@ -13,8 +14,10 @@ getWeather.addEventListener('submit', async event => {
         return;
     }
     try {
-        cityDisplay(city);
-        const weather = await getWeatherInfo(city);
+        createCityHeader(city);
+        const weather = await fetchWeatherData(city);
+        console.log(weather);
+        
         displayWeatherInfo(weather);
     } catch (error) {
         errorDisplay(error);
@@ -22,8 +25,8 @@ getWeather.addEventListener('submit', async event => {
 });
 
 
-const getWeatherInfo = async (info) => {
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${info}&appid=${apiKey}`;
+const fetchWeatherData = async (data) => {
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${apiKey}`;
     const weatherInfo = await fetch(apiURL);
     if(!weatherInfo.ok){
         throw new Error('Could not fetch weather data');
@@ -31,7 +34,7 @@ const getWeatherInfo = async (info) => {
     return await weatherInfo.json();   
 }
 
-const cityDisplay = (name) => {
+const createCityHeader = (name) => {
     const firstLetter = name.slice(0,1);
     const restOfTheLetter = name.slice(1, name.length).toLowerCase();
     const capitalizedLetter = firstLetter.toUpperCase();
@@ -45,71 +48,103 @@ const cityDisplay = (name) => {
 }
 
 const displayWeatherInfo = (info) => {
+    
     const {
         main: {humidity, temp}, 
-        weather: [{id, description}]
+        weather: [{id, description}],
+        sys: {sunset},
+        wind: {speed}
     } = info;
+    console.log(sunset, speed);
     
-    const temperature = document.createElement('p');
-    const humidityDisplay = document.createElement('p');
-    const weatherCondition = document.createElement('p');
+    const KELVIN_TO_CELSIUS = 273.15;
+    const sunsetTime = new Date(sunset * 1000);
+    const sunsetFormatedTime = sunsetTime.toLocaleTimeString([], 
+        {hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+    });
+    
     const weatherEmoji = document.createElement('p');
+    const temperature = document.createElement('p');
+    const weatherCondition = document.createElement('p');
+    const details = document.createElement('div');
 
-    temperature.textContent = `${(temp - 273.15).toFixed(2)}°C`;
-    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    weatherEmoji.textContent = emojiDisplay(weatherEmoji, id);
+    temperature.textContent = `${Math.floor(temp - KELVIN_TO_CELSIUS)}°`;
     weatherCondition.textContent = description;
-    weatherEmoji.textContent = emojiDisplay(id);
+    details.innerHTML = humidtyWindSunset('Humidity', `${humidity}%`) +
+                        humidtyWindSunset('Wind', `${speed} m/s`) +
+                        humidtyWindSunset('Sunset', sunsetFormatedTime);
     
-    temperature.classList.add('tempDisplay');
-    humidityDisplay.classList.add('humidityDisplay');
-    weatherCondition.classList.add('conditionDisplay');
     weatherEmoji.classList.add('emojiDisplay');
+    temperature.classList.add('tempDisplay');
+    weatherCondition.classList.add('conditionDisplay');
+    details.classList.add('details')
     
-    container.appendChild(temperature);
-    container.appendChild(humidityDisplay);
-    container.appendChild(weatherCondition);
     container.appendChild(weatherEmoji);
+    container.appendChild(temperature);
+    container.appendChild(weatherCondition);
+    container.appendChild(details);
 }
 
-const emojiDisplay = (weatherId) => {
+const humidtyWindSunset = (name, value) => {
+    const detailDisplay = `<p class="humidityDisplay">${name}<span>${value}</span></p>`;
+    return detailDisplay;
+}
+
+
+
+const emojiDisplay = (emoji, weatherId) => {
     switch (true) {
         case (weatherId >= 200 && weatherId <=232):
+            emoji.style.textShadow = '0 0 5px white';
             return '⛈️';
         break;
 
         case (weatherId >= 300 && weatherId <=321):
+            emoji.style.textShadow = '0 0 5px white';
             return '🌧️';
         break;
 
         case (weatherId >= 500 && weatherId <=531):
+            emoji.style.textShadow = '0 0 5px white';
             return '🌦️';
         break;
 
         case (weatherId >= 600 && weatherId <=622):
+            emoji.style.textShadow = '0 0 5px white';
             return '❄️';
         break;
 
         case (weatherId >= 701 && weatherId <=781):
+            emoji.style.textShadow = '0 0 5px white';
             return '🌪️';
         break;
 
         case (weatherId === 800):
+            emoji.style.textShadow = '0 0 5px white';
             return '🌟';
         break;
 
         case (weatherId > 800):
+            emoji.style.textShadow = '0 0 10px white';
             return '☁️';
         break;
 
         default:
-            return '��';
+            return '❓';
     }
 }
 
 const errorDisplay = (message) => {
+    // Clear existing errors
+    const existingError = document.querySelector('.errorMessage');
+    if(existingError) existingError.remove();
+
     const errorMessage = document.createElement('p');
     errorMessage.textContent = message;
     errorMessage.classList.add('errorMessage');
-    errorMessage.style.dislay = 'flex';
+    errorMessage.style.display = 'flex';
     container.appendChild(errorMessage);
 }
